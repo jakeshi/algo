061e720ceafdb8c9feb56113d625709c8d8615c1
Jake <jakehku@gmail.com>
2018-01-01 10:33:22 -0500

    Update Max_Rectangle.py

--- Max_Rectangle.py
+++ Max_Rectangle.py
@@ -1,12 +1,67 @@
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        
+        #if heights==[]:return 0
+       
+        # brutal force
+        # l = len(heights)
+        # #if l ==1: return heights[0]
+        # for i in range(l):
+        #     for j in range(i+1):
+        #         tmp = (i-j+1)*min(heights[j:i+1])
+        #         #print('i is {},j is {}, h is {},min h is {},temp is                      {}'.format(i,j,heights[j:i],min(heights[j:i+1]),tmp))
+        #         res = max(res,tmp)
+                
 
-def MaxRectangleArea(self, height):
-    height.append(0)
-    stack = [0]
-    ret = 0
-    for i in range(1, len(height)):
-        while stack and height[i] < height[stack[-1]]:
-            h = height[stack.pop()]
-            w = i if not stack else i - stack[-1] -1
-            ret = max(r, w*h)
-        stack.append(i)
-    return ret
+        heights.append(0)
+        stack = [0]
+        
+        res = 0
+        l = len(heights)
+
+        # i records the leftest position of  for each value, the value is a decreasing array
+        for i in range(1,l):
+            while stack and heights[i] < heights[stack[-1]]:
+                #print("i,stack:",i,stack)
+                h = heights[stack.pop()]
+                w = i if not stack else i - stack[-1] -1
+                
+                res = max(res, w*h)
+                #print("i,stack,h,w,res:",i,stack,h,w,res)
+                
+            stack.append(i)
+                
+        return res
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def intToString(input):
+    if input is None:
+        input = 0
+    return str(input)
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+    lines = readlines()
+    while True:
+        try:
+            line = lines.next()
+            heights = stringToIntegerList(line)
+            
+            ret = Solution().largestRectangleArea(heights)
+
+            out = intToString(ret)
+            print out
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
